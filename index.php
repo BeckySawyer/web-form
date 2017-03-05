fc47e7176309c6d425e34b7d0526616a6b32959a
BeckySawyer <yougobeckycoco@Rebeccas-MBP.atcltd.atc.org.nz>
2017-03-06 10:14:39 +1300

    added validation require function in page

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+	require 'includes/functions.php';
+
+	if($_SERVER['REQUEST_METHOD'] === 'POST') {
+		// The request is using the POST method
+		// This is where you will call the validation code
+		dd($_POST);
+	}
 	require 'partials/header.php';
 	require 'partials/form.php';
 	require 'partials/footer.php';
