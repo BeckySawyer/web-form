646f0a28ef99c6be62d7c87ad1bcd1d77810e07f
BeckySawyer <yougobeckycoco@Rebeccas-MacBook-Pro.local>
2017-03-14 21:10:11 +1300

    started css, using materialize
    couldn’t decide on bulimia, spectre or anything else. chose to stick
    with materialize.

--- partials/form.php
+++ partials/form.php
@@ -1,38 +1,63 @@
 <div class="container">
 
-<form method="POST" action="index.php" onsubmit="return validate()">
+<form method="POST" action="index.php" onsubmit="return validate()" class="form-horizontal">
 	<div class="form-group">
+	<div class="col-3">
 	<label for="name" class="form-label"> Your name  <?= !empty($errors['name']) ? $errors['name'] : ''?> </label>
+	</div>
+	<div class="col-9">
 	<input id="name" name="name" type="text" placeholder="Enter name" value="<?= !empty($name) ? $name : '' ?>" class="form-input">
 	<div id="nameError"></div>
 	</div>
+	</div>
 
 
 	<div class="form-group">
-	<label for="address"> Your address  <?= !empty($errors['address']) ? $errors['address'] : ''?> </label>
-	<textarea id="address" name="address" placeholder="Enter address"><?= !empty($address) ? $address : '' ?></textarea>
+	<div class="col-3">
+	<label for="address" class="form-label"> Your address  <?= !empty($errors['address']) ? $errors['address'] : ''?> </label>
+	</div>
+	<div class="col-9">
+	<textarea id="address" class="form-input" name="address" placeholder="Enter address"><?= !empty($address) ? $address : '' ?></textarea>
 	<div id="addressError"></div>
+	</div>
 	</div>
 	
 
-	<label for="email"> Your email <?= !empty($errors['email']) ? $errors['email'] : ''?></label>
-	<input id="email" name="email" type="text" placeholder="Enter email" value="<?= !empty($email) ? $email : '' ?>">
+	<div class="form-group">
+	<div class="col-3">
+	<label for="email" class="form-label"> Your email <?= !empty($errors['email']) ? $errors['email'] : ''?></label>
+	</div>
+	<div class="col-9">
+	<input id="email" class="form-input" name="email" type="text" placeholder="Enter email" value="<?= !empty($email) ? $email : '' ?>">
 	<div id="emailError"></div>
-
+	</div>
+	</div>
 	
 
-	<label for="age"> Your age <?= !empty($errors['age']) ? $errors['age'] : ''?></label>
-	<input id="age" name="age" type="number" min="1" placeholder="##" value="<?= !empty($age) ? $age : '' ?>">
+	<div class="form-group">
+	<div class="col-3">
+	<label for="age" class="form-label"> Your age <?= !empty($errors['age']) ? $errors['age'] : ''?></label>
+	</div>
+	<div class="col-9">
+	<input id="age" class="form-input" name="age" type="number" min="1" placeholder="##" value="<?= !empty($age) ? $age : '' ?>">
 	<div id="ageError"></div>
-
+	</div>
+	</div>
 	
 
-	<label for="dateofbirth"> Your date of birth <?= !empty($errors['dateofbirth']) ? $errors['dateofbirth'] : ''?></label>
+	<div class="form-group">
+	<div class="col-3">
+	<label for="dateofbirth" class="form-label"> Your date of birth <?= !empty($errors['dateofbirth']) ? $errors['dateofbirth'] : ''?></label>
+	</div>
+	<div class="col-9">
 	<input id="dateofbirth" name="dateofbirth" type="date" placeholder="##/##/####" value="<?= !empty($dateofbirth) ? $dateofbirth : '' ?>" onchange="calculateAge()">
+	</div>
+	</div>
+	
 
 	
 
-	<label for="movies"> Choose your favourite movie <?= !empty($errors['movies']) ? $errors['movies'] : ''?></label>
+	<label for="movies" class="form-label"> Choose your favourite movie <?= !empty($errors['movies']) ? $errors['movies'] : ''?></label>
 	<select id="movies" name="movies">
 		<option value="movie1" <?php if (!empty($_POST['movies']) && $_POST['movies'] == 'movie1') echo 'selected="selected"'; ?>> Select Movie </option>
 		<option value="movie2" <?php if (!empty($_POST['movies']) && $_POST['movies'] == 'movie2') echo 'selected="selected"'; ?>> Movie 1 </option>
@@ -42,16 +67,17 @@
 	<div id="moviesError"></div>
 
 	
+	<input type="radio" id="male" name="gender" value="male" <?php if (!empty($_POST['gender']) && $_POST['male'] == 'male') echo 'checked="checked"'; ?>>
+			<label for="male">Male<?= !empty($errors['gender']) ? $errors['gender'] : '' ?></label>
 
-	<label for="gender"> Gender <?= !empty($errors['gender']) ? $errors['gender'] : ''?></label>
-	<input id="gender" name="gender" type="radio" value="male" <?php if (!empty($_POST['gender']) && $_POST['gender'] == 'male') echo 'checked="checked"'; ?>> Male
-	<input id="gender" name="gender" type="radio" value="female" <?php if (!empty($_POST['gender']) && $_POST['gender'] == 'female') echo 'checked="checked"'; ?>> Female
-	<div id="genderError"></div>
-
+			<input type="radio" id="female" name="gender" value="female" <?php if (!empty($_POST['gender']) && $_POST['female'] == 'female') echo 'checked="checked"'; ?>>
+			<label for="female">Female<?= !empty($errors['female']) ? $errors['female'] : '' ?></label>
+	
+			<div id="genderError"></div>
 	
 
-	<button type="submit" id="submitButton"> Submit </button>
-	<button type="reset" value="reset" id="clearFields"> Clear </button>
+	<button type="submit" id="submitButton" class="btn btn-primary"> Submit </button>
+	<button type="reset" value="reset" id="clearFields" class="btn"> Clear </button>
 </form>
 
 </div>
--- partials/header.php
+++ partials/header.php
@@ -6,8 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>PHP Form</title>
 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
   <link rel="stylesheet" type="text/css" href="css/main.css">
-  <link rel="stylesheet" href="css/spectre.min.css" />
-  <link rel="stylesheet" href="css/spectre-exp.min.css" />
 </head>
 <body>
